80c4dfe3e05fdbce5ccb098f48abf53309ecf03f
RYaoYao <ryanyao13@gmail.com>
2021-01-18 17:46:28 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const handlebars = require('handlebars');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 9090;
+
+app.engine( 'hbs', exphbs({
+    extname: 'hbs', // configures the extension name to be .hbs instead of .handlebars
+    defaultView: 'main', // this is the default value but you may change it to whatever you'd like
+    layoutsDir: path.join(__dirname, '/views/layouts'), // Layouts folder
+    partialsDir: path.join(__dirname, '/views/partials'), // Partials folder
+  }));
+
+  
+  app.set('view engine', 'hbs');
+
+  app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.get('/', function(req, res) {
+    // The render function takes the template filename (no extension - that's what the config is for!)
+    // and an object for what's needed in that template
+    res.render('home', {
+    })
+});
+
+app.get('/login', function(req, res) {
+    // The render function takes the template filename (no extension - that's what the config is for!)
+    // and an object for what's needed in that template
+    res.render('login', {
+      title: 'Welcome',
+    })
+  });
+
+  app.get('/admin', function(req, res) {
+    // The render function takes the template filename (no extension - that's what the config is for!)
+    // and an object for what's needed in that template
+    res.render('admin', {
+      title: 'Welcome',
+    })
+  });
+
+  app.use(express.static('public'));
+
+// Listening to the port provided
+app.listen(port, function() {
+  console.log('App listening at port '  + port)
+});
