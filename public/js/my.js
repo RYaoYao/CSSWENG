e02a01312e6ec35278490dfb2671c40ba27f7607
RYaoYao <ryanyao13@gmail.com>
2021-01-19 16:04:53 +0800

    Added edit form modal for list of tenants

--- public/js/my.js
+++ public/js/my.js
@@ -7,9 +7,21 @@ $(document).ready( function () {
             {
                 text: 'My button',
                 action: function ( ) {
-                    alert(table.row({selected:true}).data());
+                    var edata  = table.row({selected:true}).data();
+                    console.log(table.row({selected:true}).data()[1]);
+                    myFunction(edata);
                 }
             }
         ]
   });
+function myFunction(param) {
+               $("#editname").val(param[0]);
+                    $("#editpay").val(param[1]);
+                    $("#editunit").val(param[2]);
+                    $("#editmont").val(param[3]);
+                    $("#editmos").val(param[4]);
+                    $("#editfam").val(param[5]);
+                    $("#tenantmodal").modal('show');
+  
+}
 });
